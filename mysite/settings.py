cf12cb19a26f05101150beeac1c29ce829c71a53
Lucas <hasterix69@gmail.com>
2020-06-28 23:51:18 -0300

    Debug = True

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sk@n(+od1=9=90ijcobnxgg5xyd!prb#x=8c-4)422hrdtx-7l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','hasterix.pythonanywhere.com']
 
